8ad6a46f08babede42acf11663afccb17237a8db
Jesse Heikkinen <jessehei129@gmail.com>
2020-09-17 22:36:06 +0300

    cogs poisto 2

--- remu_botti.py
+++ remu_botti.py
@@ -148,8 +148,8 @@ async def change_status():
     await client.change_presence(activity=discord.Game(next(status)))
 
 
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        client.load_extension(f"cogs.{filename[:-3]}")
+# for filename in os.listdir('./cogs'):
+#     if filename.endswith('.py'):
+#         client.load_extension(f"cogs.{filename[:-3]}")
 
 client.run('NzU2MTE4MDIyMjM3OTc4NzE1.X2NLyA.GBlNY4eNk1YVH6ewveutr-zfny4')
